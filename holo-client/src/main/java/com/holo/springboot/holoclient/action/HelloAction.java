b6fa14d6ce6e0defd607157a69d14872b96e5167
holowang <wuhancloudy@gmail.com>
2021-02-16 13:02:26 +0800

    第一次提交-commit

--- holo-client/src/main/java/com/holo/springboot/holoclient/action/HelloAction.java
+++ holo-client/src/main/java/com/holo/springboot/holoclient/action/HelloAction.java
@@ -17,7 +17,7 @@ import java.util.Map;
  * @RestController 的意思就是 Controller 里面的方法都以 json 格式输出，不用再写什么 jackjson 配置的了！
  */
 @RestController //RestController= Response+Controller
-//@Controller
+//@Controller1
 //@RequestMapping("/hello")
 public class HelloAction {
 
