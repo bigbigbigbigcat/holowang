0fa3a294d63f7f6652a7a9c9210bebf7a61de74d
holowang <wuhancloudy@gmail.com>
2021-04-20 13:25:39 +0800

    增加对于本地数据的导出

---
+++ holo-client/src/main/java/com/holo/springboot/holoclient/beans/taodaji/Specs.java
@@ -0,0 +1,16 @@
+package com.holo.springboot.holoclient.beans.taodaji;
+
+import lombok.Data;
+
+@Data
+public class Specs {
+    private String avgPrice;
+    private String avgUnit;
+    private String level1Unit;
+    private String level2Unit;
+    private String level2Value;
+    private String level3Unit;
+    private String level3Value;
+    private String levelType;
+    private String price;
+}
